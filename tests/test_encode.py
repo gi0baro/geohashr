a1fb84e5b3b79f1c920355b175aeb0367f4f0150
pquadri <pquadri10@gmail.com>
2023-07-19 12:11:15 +0200

    fix: standardize lat and lon

--- tests/test_encode.py
+++ tests/test_encode.py
@@ -6,3 +6,7 @@ from geohashr import bbox, encode, decode
 @pytest.mark.parametrize("hash", ["000000000000","zzzzzzzzzzzz","bgr96qxvpd46"])
 def test_cycle(hash):
     assert hash == encode(*decode(hash))
+
+
+def test_simple_encode():
+    assert encode(57.64911,10.40744) == 'u4pruydqqvj8'
